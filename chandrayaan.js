b3783c1f0df2faca4a00d323f50c014c81090d33
sanketsingh27 <singh.sanket27@gmail.com>
2024-03-04 23:16:09 +0530

    chandrayan move foreard in north direction

--- chandrayaan.js
+++ chandrayaan.js
@@ -1,3 +1,19 @@
-function moveChandrayaan() {}
+function moveChandrayaan({ commands, coordinates, direction }) {
+  for (let command of commands) {
+    if (command === "f") {
+      coordinates = moveForward(coordinates, direction);
+      continue;
+    }
+  }
+
+  return { coordinates, direction };
+}
+
+const moveForward = (coordinates, direction) => {
+  if ((direction = "N")) {
+    coordinates[1] += 1;
+    return coordinates;
+  }
+};
 
 module.exports = { moveChandrayaan };
--- tests/chandrayaan.test.js
+++ tests/chandrayaan.test.js
@@ -4,4 +4,19 @@ describe("move chandrayaan in (x,y,x) directions", () => {
   test("move chandrayaan function is defined", () => {
     expect(moveChandrayaan).toBeDefined();
   });
+
+  test("move chandrayaan 1 unit forward when direction is 'N", () => {
+    const initialConditions = {
+      commands: ["f"],
+      coordinates: [0, 0, 0],
+      direction: "N",
+    };
+
+    const expected = {
+      direction: "N",
+      coordinates: [0, 1, 0],
+    };
+
+    expect(moveChandrayaan(initialConditions)).toStrictEqual(expected);
+  });
 });
